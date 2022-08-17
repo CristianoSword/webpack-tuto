bb0668d0f067ac7ac965b1b0dc2a04c4634c9a83
cristiano <originalvgo@gmail.com>
2022-08-17 15:45:21 -0300

    template html plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     entry: {
@@ -9,4 +10,12 @@ module.exports = {
         path: path.resolve(__dirname, './dist'),
         filename: '[name].bundle.js',
     },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+          title: 'webpack Boilerplate',
+          template: path.resolve(__dirname, './src/template.html'), // template file
+          filename: 'index.html', // output file
+        }),
+      ],
 }
